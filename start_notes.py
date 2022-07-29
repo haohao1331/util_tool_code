edac8f8bbe27ee92aa02117b0d23a5cf07c2a094
haohao1331 <haohao1331@gmail.com>
2022-07-29 14:06:21 -0400

    open zotero obsidian vscode

---
+++ start_notes.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+notes_dir = '/Users/haohao1331/Desktop/MOK'
+
+print(os.system('open /Applications/Obsidian.app'))
+# os.chdir(notes_dir)
+# print(os.getcwd())
+os.system('open /Applications/Zotero.app')
+print(os.system('open -n "/Applications/Visual Studio Code.app"'))
+# subprocess.call(['code', '.'])
+
